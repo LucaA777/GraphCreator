8a9f3da46b7d465df249dcb67bdcc86c71d123f1
Luca Ardanaz <luca.ardanaz@gmail.com>
2026-03-19 00:23:15 -0700

    Implemented dijkstra's algorithm, could use more testing

--- GraphCreator.cpp
+++ GraphCreator.cpp
@@ -12,9 +12,11 @@ Author: Luca Ardanaz
 #include <map>
 #include <tuple>
 #include <limits>
-#include "Vertex.h"
+#include <stack>
 
 using namespace std;
+
+const bool DEBUG = false;
 
 queue<string> tokenize(string str);
 
@@ -28,8 +30,9 @@ void dijkstraPathfinding(char start, char target, set<char> vertices, set<edge> 
 
 //this is necessary since I want a set of structs for edges
 //source: https://stackoverflow.com/questions/5816658/how-to-have-a-set-of-structs-in-c
+//set duplicate control is done by checking if neither e1 < e2 nor e2 < e1
 bool operator<(const edge& e1, const edge& e2) {
-	return e1.start < e2.start;
+	return e1.start < e2.start || e1.end < e2.end;
 }
 
 int main() {
@@ -58,10 +61,11 @@ int main() {
 			cout << "Commands:" << endl;
 			cout << "av [label] - add vertex with unique label" << endl;
 			cout << "ae [vertex 1] [vertex 2] [value] - add edge between vertecies with value" << endl;
+			cout << "\tadding -d adds edge in both directions" << endl;
 			cout << "rv [label] - remove vertex by label" << endl;
 			cout << "re [vertex 1] [vertex 2] - remove edge between vertecies" << endl;
 			cout << "p - print adjacency table" << endl;
-			cout << "f - find shortext path" << endl;
+			cout << "f [start] [end] - find shortext path" << endl;
 			cout << "q - quit" << endl;
 			continue;
 		}			
@@ -104,9 +108,10 @@ int main() {
 			char start = ' ';
 			char end = ' ';
 			double val = 0.0;
+			bool bidirectional = false;
 
 			try {			
-				if (tokens.size() != 3) {
+				if (tokens.size() != 3 && tokens.size() != 4) {
 					//throws a random number if the size doesn't match
 					throw 1000;
 				}
@@ -115,7 +120,18 @@ int main() {
 				tokens.pop();
 				end = tokens.front()[0];
 				tokens.pop();
-				val = stod(tokens.front()); 
+				val = stod(tokens.front());
+			       	tokens.pop();
+
+				if (!tokens.empty()) {
+					bidirectional = tokens.front() == "-d";
+
+					//if not bidirectional, but there are 4 tokens, error
+					if (!bidirectional) {
+						throw 2000;
+					}	
+				}
+					
 			}
 			catch(...) {
 				cout << "Invalid tokens for command 'ae'" << endl;
@@ -136,6 +152,12 @@ int main() {
 					duplicate = true;
 					break;
 				}
+
+				if (bidirectional && e.start == end && e.end == start) {
+					cout << "Duplicate edge, aborted" << endl;
+					duplicate = true;
+					break;
+				}
 			}
 
 			if (duplicate) {
@@ -143,7 +165,19 @@ int main() {
 			}
 
 			//add the edge
+			if (DEBUG) {
+				cout << "Inserting edge from " << start << " to " << end << " with value: " << val << endl;
+			}
 			edges.insert({start, end, val});
+
+			//insert in opposite direction aswell if bidirectional
+			if (bidirectional) {
+				if (DEBUG) {
+					cout << "Inserting edge from " << end << " to " << start << " with value: " << val << endl;
+				}
+
+				edges.insert({end, start, val});
+			}
 
 		}	
 
@@ -290,6 +324,53 @@ int main() {
 			continue;
 		}	
 
+		if (tokens.front() == "f") {
+			tokens.pop();
+
+			//get labels
+			char start = ' ';
+			char end = ' ';
+
+			try {			
+				if (tokens.size() != 2) {
+					//throws a random number if the size doesn't match
+					throw 1000;
+				}
+
+				start = tokens.front()[0];
+				tokens.pop();
+				end = tokens.front()[0];
+			}
+			catch(...) {
+				cout << "Invalid tokens for command 'f'" << endl;
+				continue;
+			}
+
+			//ensure vertex validity
+			if (start == ' ' || end == ' ' || start == end) {
+				cout << "Invalid vertices" << endl;
+				continue;
+			}
+
+			//run pathfinding
+			if (DEBUG) {
+				cout << "Starting pathfinding..." << endl;
+			}
+
+			dijkstraPathfinding(start, end, vertices, edges);
+			
+			if (DEBUG) {
+				cout << "Finished pathfinding" << endl;
+			}
+
+			continue;
+
+		}	
+
+		//no matching command
+		cout << "No matching command" << endl;
+
+
 	} while (tokens.front() != "q");	
 
 	return 0;
@@ -314,34 +395,20 @@ queue<string> tokenize(string str) {
 	return tokens;
 }
 
-Vertex* getVertexByLabel(char label, vector<Vertex*> vertices) {
-	for (Vertex* v : vertices) {
-		if (v -> getLabel() == label) {
-			return v;
-		}	
-	}
-
-	return nullptr;
-}
-
-int getVertexIndex(Vertex* vertex, vector<Vertex*> vertices) {
-	//find match, return the index
-	for (int i = 0; i < vertices.size(); i++) {
-		if (vertices.at(i) == vertex) {
-			return i;
-		}
-	}
-
-	//returns -1 if no match was found
-	return -1;
-}
-
 void dijkstraPathfinding(char start, char target, set<char> vertices, set<edge> edges) {
+
+	if (DEBUG) {
+		cout << "Creating lists and table..." << endl;
+	}
 
 	//set queues up and create map to track shortest paths
 	set<char> visited;
 	set<char> unvisited;	
 	map<char, tuple<double, char>> paths;
+
+	if (DEBUG) {
+		cout << "Adding vertices to lists and table..." << endl;
+	}
 
 	for (char v : vertices) {
 
@@ -354,10 +421,21 @@ void dijkstraPathfinding(char start, char target, set<char> vertices, set<edge> 
 		paths[v] = make_tuple((v == start ? 0 : numeric_limits<double>::max()), ' ');
 	}
 
+	if (DEBUG) {
+		cout << "Starting algorithm..." << endl;
+	}
+
+	char current = start;
 
 	//begin algorithm
 	while (!unvisited.empty()) {
-		char current = start;
+
+		unvisited.erase(current);
+
+		if (DEBUG) {
+			cout << "At vertex " << current << endl;
+			cout << "Getting neighbors..." << endl;
+		}
 
 		//get unvisited neighbors
 		queue<edge> neighbors;
@@ -367,20 +445,91 @@ void dijkstraPathfinding(char start, char target, set<char> vertices, set<edge> 
 			// - the edge ends at an unvisited vertex 
 			if (e.start == current && unvisited.count(e.end)) {
 				neighbors.push(e);
+
+				if (DEBUG) {
+					cout << "Found unvisited neighbor " << e.end << endl;
+				}
 			}
 		}	
+
+		if (DEBUG) {
+			cout << "Calculating path lengths..." << endl;
+		}
 
 		//for each neighbor, check and update paths
 		while (!neighbors.empty()) {
 			//store the current neighbor
-			char neighbor = neighbors.front();
+			edge neighbor = neighbors.front();
 			neighbors.pop();
 
 			//calculate the path length
+			double length = get<0>(paths[current]) + neighbor.value;
 
+			if (DEBUG) {
+				cout << "Path from start to " << neighbor.end << " is " << length << endl;
+			}
 
-		}	
+			//store length if shortest
+			if (length < get<0>(paths[neighbor.end])) {
+				paths[neighbor.end] = make_tuple(length, current);
+			}
+
+		}
+
+		if (DEBUG) {
+			cout << "Picking next vertex..." << endl;
+		}
+
+		//go to the unvisited vertex with the shortest current path
+		//add current vertex to visited
+		visited.insert(current);
+
+		char shortest = ' ';
+		for (char v : unvisited) {
+			
+			//check for new shortest
+			if (shortest == ' ' || get<0>(paths[v]) < get<0>(paths[shortest])) {
+				shortest = v;
+			}			
+		}
+	
+		current = shortest;
+
+		if (DEBUG && !unvisited.empty()) {
+			cout << "Continuing to next vertex..." << endl;
+		}
 	}
+
+	if (DEBUG) {
+		cout << "Isolating shortest path..." << endl;
+	}
+
+	//get results
+	
+	//if the shortest path length is still max, then there is no path
+	if (get<0>(paths[target]) == numeric_limits<double>::max()) {
+		cout << "No connecting path" << endl;
+		return;
+	}
+
+	stack<char> shortestPath;
+	current = target;
+	
+	//backtrack through the table to see which path leads to the target
+	while (current != start) {
+		current = get<1>(paths[current]);
+		shortestPath.push(current);
+	}
+
+	cout << "Shortest path: ";
+	while (!shortestPath.empty()) {
+		cout << shortestPath.top() << " -> ";
+		shortestPath.pop();
+	}
+
+	cout << target << endl; 
+	
+	cout << "Shortest path length: " << get<0>(paths[target]) << endl;
 
 }
 
